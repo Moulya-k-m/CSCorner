b8113f46f54bd086604173548f764af1d047a0fc
Moulya K M <kmmoulya3@gmail.com>
2026-07-22 20:15:10 +0530

    New

---
+++ Python/OOPS/Student.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+
+s1=student("Mohan",97)
+print(s1.name,s1.marks)
+
+s2=student("Rohan",98)
+print(s2.name,s2.marks)
+
